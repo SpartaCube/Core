f0759f92373bbc9395d0001b79c429604cdd67ca
Iban <iban.etche@outlook.fr>
2021-02-07 19:03:47 +0100

    Opti xp -> async

--- SurvivalCore/src/main/java/fr/iban/survivalcore/listeners/PlayerExpChangeListener.java
+++ SurvivalCore/src/main/java/fr/iban/survivalcore/listeners/PlayerExpChangeListener.java
@@ -1,63 +1,18 @@
 package fr.iban.survivalcore.listeners;
 
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.concurrent.ConcurrentHashMap;
-
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerExpChangeEvent;
 
-import fr.iban.common.data.AccountProvider;
-import fr.iban.spartacube.data.Account;
-import fr.iban.survivalcore.SurvivalCorePlugin;
-import fr.iban.survivalcore.utils.LevelUtils;
+import fr.iban.survivalcore.utils.XPProvider;
 
 public class PlayerExpChangeListener implements Listener {
-
-	public Map<Player, Map<Long, Integer>> xpLogs = new ConcurrentHashMap<>();
 
 	@EventHandler
 	public void onExpChange(PlayerExpChangeEvent e) {
 		Player player = e.getPlayer();
-		AccountProvider ac = new AccountProvider(player.getUniqueId());
-		Account account = ac.getAccount();
-		if(!SurvivalCorePlugin.ess.getUser(player.getUniqueId()).isAfk()) {
-			if(checkLast(player) < 6) {
-				short levelbefore = account.getLevel();
-				account.addExp((e.getAmount() < 12 ? e.getAmount() : 12));
-				getXPLogs(player).put(System.currentTimeMillis(), e.getAmount());
-				short levelafter = account.getLevel();				
-				e.getPlayer().sendActionBar(LevelUtils.getLevelProgressBar(account, 20));
-				if(levelbefore < levelafter)
-					LevelUtils.sendLevelUpReward(account, levelbefore, levelafter);
-			}
-		}else {
-			e.setAmount(0);
-		}
-		ac.sendAccountToRedis(account);
-	}
-
-	public Map<Long, Integer> getXPLogs(Player player){
-		if(!xpLogs.containsKey(player)) {
-			xpLogs.put(player, new ConcurrentHashMap<>());
-		}
-		return xpLogs.get(player);
-	}
-
-	private int checkLast(Player player) {
-		int amountLastMin = 0;
-		if(!getXPLogs(player).isEmpty()) {
-			for(Entry<Long, Integer> log : getXPLogs(player).entrySet()) {
-				if(System.currentTimeMillis() - log.getKey() > 6000) {
-					getXPLogs(player).remove(log.getKey());
-				}else {
-					amountLastMin += log.getValue();
-				}
-			}
-		}
-		return amountLastMin;
+		XPProvider.addXP(player, e.getAmount(), true);
 	}
 
 }
---
+++ SurvivalCore/src/main/java/fr/iban/survivalcore/utils/XPProvider.java
@@ -0,0 +1,62 @@
+package fr.iban.survivalcore.utils;
+
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.bukkit.entity.Player;
+
+import fr.iban.common.data.AccountProvider;
+import fr.iban.spartacube.data.Account;
+
+public class XPProvider {
+
+	private static Map<Player, Map<Long, Integer>> xpLogs = new ConcurrentHashMap<>();
+
+	public static CompletableFuture<Void> addXP(Player player, int amount, boolean applynerf) {
+		return CompletableFuture.runAsync(() -> {
+			AccountProvider ap = new AccountProvider(player.getUniqueId());
+			Account account = ap.getAccount();
+			if(applynerf && checkLast(player, 6) > 12) {
+				return;
+			}
+
+			short levelbefore = account.getLevel();
+			account.addExp(amount > 12 ? 12 : amount);
+			short levelafter = account.getLevel();
+
+			player.sendActionBar(LevelUtils.getLevelProgressBar(account, 20));
+
+			if(levelbefore < levelafter)
+				LevelUtils.sendLevelUpReward(account, levelbefore, levelafter);
+
+			ap.sendAccountToRedis(account);
+
+			//On log l'xp ajouté
+			getXPLogs(player).put(System.currentTimeMillis(), amount);
+		});
+	}
+
+	private static Map<Long, Integer> getXPLogs(Player player){
+		if(!xpLogs.containsKey(player)) {
+			xpLogs.put(player, new ConcurrentHashMap<>());
+		}
+		return xpLogs.get(player);
+	}
+
+	private static int checkLast(Player player, int periodInSec) {
+		int amountLastTenMin = 0;
+		if(!getXPLogs(player).isEmpty()) {
+			for(Entry<Long, Integer> log : getXPLogs(player).entrySet()) {
+				if(System.currentTimeMillis() - log.getKey() > 1000*periodInSec) {
+					getXPLogs(player).remove(log.getKey());
+				}else {
+					amountLastTenMin += log.getValue();
+				}
+			}
+		}
+		return amountLastTenMin;
+	}
+
+}
