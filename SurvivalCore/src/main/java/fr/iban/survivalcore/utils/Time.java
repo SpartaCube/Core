d13c8267f1d8f8600aa12d04f1174072d3efa1ff
Jay <benkhalil100@yahoo.ca>
2021-02-21 10:39:03 -0500

    Refonte du Time utils

--- SurvivalCore/src/main/java/fr/iban/survivalcore/utils/Time.java
+++ SurvivalCore/src/main/java/fr/iban/survivalcore/utils/Time.java
@@ -4,23 +4,28 @@ import java.util.concurrent.TimeUnit;
 
 public class Time {
 	
-	public static String calculateTime(long seconds) {
-        int day = (int) TimeUnit.SECONDS.toDays(seconds);
-        long hours = TimeUnit.SECONDS.toHours(seconds) - (day * 24);
-        long minute = TimeUnit.SECONDS.toMinutes(seconds) - (TimeUnit.SECONDS.toHours(seconds) * 60);
-        long second = TimeUnit.SECONDS.toSeconds(seconds) - (TimeUnit.SECONDS.toMinutes(seconds) * 60);
-        
-        if(seconds <= 61) {
-        	return second + " secondes";
-        } else if(seconds > 60) {
-        	return minute + " minutes " + second + " secondes";
-        } else if(seconds > 3600) {
-        	return hours + " heures " + minute + " minutes " + second + " secondes";
-        } else if(seconds > 86400) {
-        	return day + " jours " + hours + " heures " + minute + " minutes " + second + " secondes";
-        }
-        return day + " jours " + hours + " heures " + minute + " minutes " + second + " secondes";
+	public static String calculateTime(long secondes) {
+        int days = (int) TimeUnit.SECONDS.toDays(secondes);
+        long hours = TimeUnit.SECONDS.toHours(secondes)%24;
+        long minutes = TimeUnit.SECONDS.toMinutes(secondes)%60;
+        long seconds = secondes%60;
 
+        StringBuilder sb = new StringBuilder();
+
+        if(days > 0) {
+            sb.append(" " + days + (days == 1 ? " jour " : " jours "));
+        }
+        if(hours > 0) {
+            sb.append(hours + (hours == 1 ? " heure " : " heures "));
+        }
+        if(minutes > 0) {
+            sb.append(minutes + (minutes == 1 ? " minute " : " minutes "));
+        }
+        if(seconds > 0) {
+            sb.append(seconds + (seconds == 1 ? " seconde " : " secondes "));
+        }
+
+        return sb.toString();
     }
 
 }
